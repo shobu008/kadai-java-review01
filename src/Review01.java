6484195d5235ab50cebabb96337c106f6f4a707f
shobu008 <shobu.okamoto@gmail.com>
2023-01-29 19:41:32 +0900

    課題の再再提出

--- src/Review01.java
+++ src/Review01.java
@@ -2,15 +2,15 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        int s = 1500;
-        int t = 10;
+        double s = 1500;
+        double t = 0.1;
         double tax = tax(s,t);
         double money = tax(s,t) + s;
         System.out.println(s + "円の商品の税込価格は" + money + "円(消費税は" + tax + "円)です。");
     }
 
-    public static double tax(int s, int t) {
-        int result = s/t;
+    public static double tax(double s, double t) {
+        double result = s*t;
         return result;
     }
 }
